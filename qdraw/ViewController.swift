35772bb8b0cd66c8c702131172620b156a989d14
masaemon <masaemonbombardier@gmail.com>
2019-09-28 11:11:47 +0900

    Initial Commit

---
+++ qdraw/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  qdraw
+//
+//  Created by masaemon on 9/28/19.
+//  Copyright © 2019 masaemon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ qdrawTests/qdrawTests.swift
@@ -0,0 +1,34 @@
+//
+//  qdrawTests.swift
+//  qdrawTests
+//
+//  Created by masaemon on 9/28/19.
+//  Copyright © 2019 masaemon. All rights reserved.
+//
+
+import XCTest
+@testable import qdraw
+
+class qdrawTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
